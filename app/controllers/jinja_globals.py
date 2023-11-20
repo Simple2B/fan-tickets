77fe26e6a18c0bee56aad60b117b07da98f34c3e
denys <denysburimov@gmai.com>
2023-11-20 21:24:50 +0200

    feat: pictures stream

--- app/controllers/jinja_globals.py
+++ app/controllers/jinja_globals.py
@@ -1,7 +1,9 @@
 import os
+import base64
 import pytz
 from datetime import datetime
 from flask_wtf import FlaskForm
+from app import models as m, db
 
 
 def form_hidden_tag():
@@ -22,3 +24,11 @@ def time_delta(created_at: datetime):
 
 def cut_seconds(created_at: datetime):
     return created_at.strftime("%Y-%m-%d %H:%M")
+
+
+def stream_picture(picture_id):
+    picture_query = m.Picture.select().where(m.Picture.id == picture_id)
+    picture = db.session.scalar(picture_query)
+    img_byte_arr = picture.file
+    base64_img = base64.b64encode(img_byte_arr).decode("utf-8")
+    return base64_img
