8fc2bc2ac1756a8815fc63216d5ec23f01997c48
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-11-27 23:14:25 +0200

    deleted comments

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,6 @@ module.exports = {
         'slider-rio': "url('/static/img/slider/rio.png')",
         'slider-san-paulo': "url('/static/img/slider/san_paulo.png')",
         'slider-rio': "url('/static/img/slider/florianopolis.png')",
-        // footer: "url('/static/img/background_footer.png')",
       },
       borderRadius: {
         '50%': '50%',
