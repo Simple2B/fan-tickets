5efd903512044697e1fe4e09ae8a306b81322fa7
denys <denysburimov@gmai.com>
2023-11-29 08:50:46 +0200

    fix: csrf_token

--- app/views/users.py
+++ app/views/users.py
@@ -133,10 +133,17 @@ def user_profile():
     user: m.User = current_user
     payments_query = m.Payment.select().where(m.Payment.buyer_id == user.id)
     payments = db.session.scalars(payments_query).all()
+
+    email_form = f.EmailEditForm()
+    phone_form = f.PhoneEditForm()
+    card_form = f.CardEditForm()
     return render_template(
-        "user/profile_landing.html",
+        "user/profile.html",
         user=user,
         payments=payments,
+        email_form=email_form,
+        phone_form=phone_form,
+        card_form=card_form,
     )
 
 
