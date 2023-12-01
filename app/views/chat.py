4e98d97c1739093693dfc93028aaaaab2d9fb2cc
denys <denysburimov@gmai.com>
2023-12-01 18:24:37 +0200

    work: doesnt work

--- app/views/chat.py
+++ app/views/chat.py
@@ -1,14 +1,11 @@
-from typing import Any
 from datetime import datetime
-from flask import request, Blueprint, render_template
-from flask_login import login_required, current_user
+from flask import request, Blueprint, render_template, flash
+from flask_login import current_user
 from app import models as m, db
-from app import schema as s
-
+from app.logger import log
 from config import config
 
 CFG = config()
-
 
 chat_blueprint = Blueprint("chat", __name__, url_prefix="/chat")
 
@@ -38,7 +35,7 @@ def sell():
     db.session.commit()
 
     return render_template(
-        "chat/chat_01.html",
+        "chat/chat_01_username.html",
         now=now_str,
         room=room,
     )
@@ -81,15 +78,144 @@ def username():
     db.session.commit()
 
     return render_template(
-        "chat/chat_02.html",
+        "chat/chat_02_email.html",
         now=now_str,
         room=room,
         user=user,
     )
 
 
+@chat_blueprint.route("/email", methods=["GET", "POST"])
+def email():
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M")
+
+    room_unique_id = request.args.get("room_unique_id")
+    email = request.args.get("chat_email")
+    user_unique_id = request.args.get("user_unique_id")
+
+    user_query = m.User.select().where(m.User.unique_id == user_unique_id)
+    user: m.User = db.session.scalar(user_query)
+
+    room_query = m.Room.select().where(m.Room.unique_id == room_unique_id)
+    room: m.Room = db.session.scalar(room_query)
+
+    m.Message(
+        sender_id=2,
+        room_id=room.id,
+        text="Please input your email",
+    ).save(False)
+    m.Message(
+        room_id=room.id,
+        text=email,
+    ).save(False)
+    user.email = str(email)  # mypy made me do it!
+    db.session.commit()
+
+    return render_template(
+        "chat/chat_03_pass.html",
+        now=now_str,
+        room=room,
+        user=user,
+    )
+
+
+@chat_blueprint.route("/password", methods=["GET", "POST"])
+def password():
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M")
+
+    room_unique_id = request.args.get("room_unique_id")
+    user_unique_id = request.args.get("user_unique_id")
+    password = request.args.get("chat_password")
+    confirm_password = request.args.get("chat_confirm_password")
+
+    user_query = m.User.select().where(m.User.unique_id == user_unique_id)
+    user: m.User = db.session.scalar(user_query)
+
+    room_query = m.Room.select().where(m.Room.unique_id == room_unique_id)
+    room: m.Room = db.session.scalar(room_query)
+
+    if password != confirm_password:
+        return render_template(
+            "chat/chat_03_pass.html",
+            now=now_str,
+            room=room,
+            user=user,
+            error="Passwords don't match",
+        )
+
+    m.Message(
+        sender_id=2,
+        room_id=room.id,
+        text="Please input your password",
+    ).save(False)
+    m.Message(
+        room_id=room.id,
+        text=password,
+    ).save(False)
+    user.password = password
+    db.session.commit()
+
+    return render_template(
+        "chat/chat_04_phone.html",
+        now=now_str,
+        room=room,
+        user=user,
+    )
+
+
+@chat_blueprint.route("/phone", methods=["GET", "POST"])
+def phone():
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M")
+
+    room_unique_id = request.args.get("room_unique_id")
+    phone = request.args.get("chat_phone")
+    user_unique_id = request.args.get("user_unique_id")
+
+    user_query = m.User.select().where(m.User.unique_id == user_unique_id)
+    user: m.User = db.session.scalar(user_query)
+
+    room_query = m.Room.select().where(m.Room.unique_id == room_unique_id)
+    room: m.Room = db.session.scalar(room_query)
+
+    m.Message(
+        sender_id=2,
+        room_id=room.id,
+        text="Please input your phone",
+    ).save(False)
+    m.Message(
+        room_id=room.id,
+        text=phone,
+    ).save(False)
+    user.phone = str(phone)  # mypy made me do it!
+    db.session.commit()
+
+    return render_template(
+        "chat/chat_03_pass.html",
+        now=now_str,
+        room=room,
+        user=user,
+    )
+
+
+@chat_blueprint.route("/send-code", methods=["GET", "POST"])
+def send_code():
+    try:
+        log(log.INFO, "Verification code sent successfully")
+        flash("Verification code sent successfully")
+        return {"status": "success"}, 200
+    except Exception as e:
+        log(log.ERROR, "Error sending verification code: [%s]", e)
+        flash("Error sending verification code")
+        return {"status": "error"}, 500
+
+
 @chat_blueprint.route("/check", methods=["GET", "POST"])
 def check():
+    verification_code = request.args.get("verification_code")
+
     return render_template(
         "chat/chat_check.html",
     )
