31f1766575c2a7a353a70576b8f20f6cc79abcb0
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-12-05 15:32:59 +0200

    fix: styles for tickets

---
+++ app/static/js/chat.d.ts
@@ -0,0 +1,4 @@
+declare const observer: MutationObserver;
+declare const chatForm: Element;
+declare const chatInput: Element;
+declare const chatMessages: Element;
--- src/chat.ts
+++ src/chat.ts
@@ -10,4 +10,23 @@ document.addEventListener('DOMContentLoaded', () => {
     chatWindow.classList.toggle('chat-window-close');
     chatWindow.classList.toggle('chat-window-open');
   });
+  console.log('chat.ts loaded');
 });
+
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (mutation.type === 'childList') {
+      const chatWindow = document.querySelector('#chat-window');
+      const chatMessages = chatWindow.querySelector('.chat-messages');
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+  });
+});
+
+observer.observe(document.querySelector('.chat-messages'), {
+  childList: true,
+});
+
+const chatForm = document.querySelector('#chat-form');
+const chatInput = document.querySelector('#chat-input');
+const chatMessages = document.querySelector('.chat-messages');
