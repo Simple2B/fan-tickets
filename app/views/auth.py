57d8743a57eaa824953fa9656f44ee9bd5290034
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2023-12-07 18:23:26 +0200

    fix: delete comments

--- app/views/auth.py
+++ app/views/auth.py
@@ -1,7 +1,6 @@
 import os
 from random import randint
 from twilio.rest import Client
-from urllib.parse import urlparse
 from flask_mail import Message
 from flask import Blueprint, render_template, url_for, redirect, flash, request, session
 from flask import current_app as app
