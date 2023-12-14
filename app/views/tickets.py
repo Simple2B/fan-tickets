9f50bcf75d9674924abb67561dbd78b40b082e12
denys <denysburimov@gmai.com>
2023-12-14 15:40:46 +0200

    search fix

--- app/views/tickets.py
+++ app/views/tickets.py
@@ -19,40 +19,26 @@ def get_all_tickets(filter: s.TicketFilter) -> list[m.Ticket]:
         tickets_query = tickets_query.where(m.Ticket.event_id == filter.event_id)
     else:
         if filter.q:
-            tickets_query = tickets_query.where(
-                m.Ticket.event.has(m.Event.name.ilike(f"%{filter.q}%"))
-            )
+            tickets_query = tickets_query.where(m.Ticket.event.has(m.Event.name.ilike(f"%{filter.q}%")))
 
         if filter.location:
-            location_query = m.Location.select().where(
-                m.Location.name == filter.location
-            )
+            location_query = m.Location.select().where(m.Location.name == filter.location)
             location = db.session.scalar(location_query)
-            tickets_query = tickets_query.where(
-                m.Ticket.event.has(location_id=location.id)
-            )
+            tickets_query = tickets_query.where(m.Ticket.event.has(location_id=location.id))
 
         if filter.categories:
             tickets_query = tickets_query.filter(
-                m.Ticket.event.has(
-                    m.Event.category.has(m.Category.name.in_(filter.categories))
-                )
+                m.Ticket.event.has(m.Event.category.has(m.Category.name.in_(filter.categories)))
             )
 
         if filter.date_from:
             date_from = datetime.strptime(filter.date_from, CFG.DATE_PICKER_FORMAT)
-            tickets_query = tickets_query.where(
-                m.Ticket.event.has(m.Event.date_time >= date_from)
-            )
+            tickets_query = tickets_query.where(m.Ticket.event.has(m.Event.date_time >= date_from))
         if filter.date_to:
             date_to = datetime.strptime(filter.date_to, CFG.DATE_PICKER_FORMAT)
-            tickets_query = tickets_query.where(
-                m.Ticket.event.has(m.Event.date_time <= date_to)
-            )
+            tickets_query = tickets_query.where(m.Ticket.event.has(m.Event.date_time <= date_to))
 
-    return db.session.scalars(
-        tickets_query.limit(filter.tickets_per_page + CFG.TICKETS_PER_PAGE)
-    ).all()
+    return db.session.scalars(tickets_query.limit(filter.tickets_per_page + CFG.TICKETS_PER_PAGE)).all()
 
 
 @tickets_blueprint.route("/", methods=["GET", "POST"])
