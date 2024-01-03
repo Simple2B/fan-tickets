0ee3c1ab1707f1d4c07409832aa1cc6544ba499e
denys <denysburimov@gmai.com>
2024-01-03 15:31:32 +0200

    feat: start dispute

--- app/views/chat_disputes.py
+++ app/views/chat_disputes.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_required
 from flask_mail import Message
 from flask_sse import sse
 from app import models as m, db, mail
-from app.controllers import utcnow_chat_format, save_message
+from app.controllers import utcnow_chat_format
 from app.logger import log
 from config import config
 
@@ -16,11 +16,10 @@ chat_disputes_blueprint = Blueprint("disputes", __name__, url_prefix="/disputes"
 @chat_disputes_blueprint.route("/", methods=["GET"])
 def start_dispute():
     payment_id = request.args.get("payment_id")
-
     payment_query = m.Payment.select().where(m.Payment.id == payment_id)
     payment: m.Payment = db.session.scalar(payment_query)
-
     room_query = m.Room.select().where(m.Room.ticket_id == payment.ticket_id)
+
     room: m.Room = db.session.scalar(room_query)
 
     if not room:
@@ -29,27 +28,49 @@ def start_dispute():
             seller_id=payment.ticket.seller_id,
             buyer_id=current_user.id,
             ticket_id=payment.ticket_id,
-        ).save()
+        ).save(False)
+        db.session.flush()
+        m.Message(
+            sender_id=app.config["CHAT_DEFAULT_BOT_ID"],
+            room_id=room.id,
+            text="If you want to start a dispute, please describe the issue",
+        ).save(False)
+        db.session.commit()
 
-    # save_message(
-    #     "Message from the bot",
-    #     "Message from the user",
-    #     # room,
-    # )
+        return render_template(
+            "admin/start_dispute.html",
+            now=utcnow_chat_format(),
+            room=room,
+        )
 
     return render_template(
-        "disputes/start_dispute.html",
+        "admin/messages.html",
         now=utcnow_chat_format(),
         room=room,
     )
 
 
-@chat_disputes_blueprint.route("/send/<room_unique_id>")
+@chat_disputes_blueprint.route("/send")
 @login_required
 def send_message():
-    sse.publish(
-        {"message": "Hello!"},
-        type="greeting",
-        channel="room_unique_id",
+    # sse.publish(
+    #     {"message": "Hello!"},
+    #     type="greeting",
+    #     channel="room_unique_id",
+    # )
+    room_unique_id = request.args.get("room_unique_id")
+    user_message = request.args.get("user_message")
+    if room_unique_id:
+        room_query = m.Room.select().where(m.Room.unique_id == room_unique_id)
+        room: m.Room = db.session.scalar(room_query)
+    if user_message:
+        m.Message(
+            room_id=room.id,
+            sender_id=current_user.id,
+            text=user_message,
+        ).save()
+    return render_template(
+        "admin/messages.html",
+        now=utcnow_chat_format(),
+        room=room,
     )
-    return "Message sent!"
