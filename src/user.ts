e15859f3507c0e0bb878baeec807acd3784fb5d9
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-01-17 18:58:45 +0200

    work: fix user search button

--- src/user.ts
+++ src/user.ts
@@ -69,9 +69,8 @@ const searchInput: HTMLInputElement = document.querySelector(
 const searchInputButton = document.querySelector('#table-search-user-button');
 if (searchInputButton && searchInput) {
   searchInputButton.addEventListener('click', () => {
-    const url = new URL(window.location.href);
-    url.searchParams.set('q', searchInput.value);
-    window.location.href = `${url.href}`;
+    searchInput.value = '';
+    searchInput.click();
   });
 }
 const deleteButtons = document.querySelectorAll('.delete-user-btn');
