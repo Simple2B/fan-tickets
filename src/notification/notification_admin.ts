7a7cb5116503cb20cb7fc059ef2af4d6c9ff79c8
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-02-08 16:51:09 +0200

    work: add type to htmx, remove migrations

--- src/notification/notification_admin.ts
+++ src/notification/notification_admin.ts
@@ -1,16 +1,21 @@
-import { Notification } from './schema';
+import * as htmx from 'htmx.org';
+import {Notification} from './schema';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const channelQueryParameters = new URLSearchParams({
-        channel: "admin",
-    });
+  const channelQueryParameters = new URLSearchParams({
+    channel: 'admin',
+  });
 
-    const eventSource = new EventSource('/sse'.concat("?", channelQueryParameters.toString()));
+  const eventSource = new EventSource(
+    '/sse'.concat('?', channelQueryParameters.toString()),
+  );
 
-    const newNotificationInput = document.querySelector('input[name="notification_uuid"]') as HTMLInputElement;
-    eventSource.onmessage = (evt) => {
-        const notificationData: Notification = JSON.parse(evt.data);
-        newNotificationInput.value = notificationData.uuid;
-        htmx.trigger('#notification_loader', "load_notification");
-    }
+  const newNotificationInput = document.querySelector(
+    'input[name="notification_uuid"]',
+  ) as HTMLInputElement;
+  eventSource.onmessage = evt => {
+    const notificationData: Notification = JSON.parse(evt.data);
+    newNotificationInput.value = notificationData.uuid;
+    htmx.trigger('#notification_loader', 'load_notification');
+  };
 });
