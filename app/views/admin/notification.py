9db63ab4f7e8e753107308a9c9ce5f7b80ad8435
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-02-13 18:49:33 +0200

    work: add notifications

--- app/views/admin/notification.py
+++ app/views/admin/notification.py
@@ -3,7 +3,7 @@ import sqlalchemy as sa
 from http import HTTPStatus
 
 from flask import render_template, Blueprint, request, abort
-from flask_login import current_user, login_required
+from flask_login import current_user
 
 from app import models as m
 from app.database import db
@@ -11,11 +11,10 @@ from app.database import db
 
 NOTIFICATIONS_PER_PAGE = 5
 
-notification_blueprint = Blueprint("notification", __name__, url_prefix="/notification")
+notification_blueprint_admin = Blueprint("notification", __name__, url_prefix="/notification")
 
 
-@notification_blueprint.route("/", methods=["GET"])
-@login_required
+@notification_blueprint_admin.route("/", methods=["GET"])
 def get_notifications():
     page = request.args.get("page", 1, type=int)
     total_notifications_count = db.session.scalar(
@@ -35,15 +34,23 @@ def get_notifications():
 
     return render_template(
         "notification/notifications.html",
-        notifications=notifications,
+        notifications=reversed(notifications.all()),
         page=page,
     )
 
 
-@notification_blueprint.route("/notification_list", methods=["GET"])
-@login_required
-def notification_list():
-    notifications_count = db.session.scalar(
-        sa.select(sa.func.count(m.Notification.id)).where(m.Notification.users.any(m.User.id == current_user.id))
-    )
-    return render_template("user/notification/notifications.html", notifications_count=notifications_count)
+@notification_blueprint_admin.route("/get_notification", methods=["GET"])
+def get_notification():
+    notification_uuid = request.args.get("notification_uuid")
+
+    if not notification_uuid:
+        abort(HTTPStatus.NOT_FOUND)
+
+    where_stmt = [m.Notification.uuid == notification_uuid]
+
+    notification = db.session.scalar(sa.select(m.Notification).where(*where_stmt))
+
+    if not notification:
+        abort(HTTPStatus.NOT_FOUND)
+
+    return render_template("notification/notification.html", notification=notification)
