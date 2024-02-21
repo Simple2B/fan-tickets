cce5538a7de1c0a784ab6a74ea36d4ff6c2360be
denys <denysburimov@gmai.com>
2024-02-21 20:31:38 +0200

    fix: static

--- src/profile.ts
+++ src/profile.ts
@@ -1,16 +1,19 @@
-import { setMessageSendUrl, createChatWindow, setNewRoomUuid } from "./chat";
-import { listenNewRoom } from "./dispute";
+import {setMessageSendUrl, createChatWindow, setNewRoomUuid} from './chat';
+import {listenNewRoom} from './dispute';
 
 setMessageSendUrl('/disputes/send');
 
 document.addEventListener('DOMContentLoaded', () => {
-    const startDisputeButtonList = document.querySelectorAll('.start-dispute-btn');
-    startDisputeButtonList.forEach(button => button.addEventListener('click', async () => {
-        const paymentId = button.getAttribute('data-payment-id');
-        const resp = await fetch('/disputes/?payment_id='.concat(paymentId));
-        const roomUuid = await resp.text();
-        createChatWindow();
-        listenNewRoom(roomUuid);
-        setNewRoomUuid(roomUuid);
-    }));
+  const startDisputeButtonList =
+    document.querySelectorAll('.start-dispute-btn');
+  startDisputeButtonList.forEach(button =>
+    button.addEventListener('click', async () => {
+      createChatWindow();
+      const paymentId = button.getAttribute('data-payment-id');
+      const resp = await fetch('/disputes/?payment_id='.concat(paymentId));
+      const roomUuid = await resp.text();
+      listenNewRoom(roomUuid);
+      setNewRoomUuid(roomUuid);
+    }),
+  );
 });
