baf3ad5134b43ce1824af70309371f3eda1af583
AndreyBerezyanskiy <andrey.berezyanskiy@gmail.com>
2024-02-22 09:23:49 +0200

    fix: static

--- src/profile.ts
+++ src/profile.ts
@@ -8,7 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.start-dispute-btn');
   startDisputeButtonList.forEach(button =>
     button.addEventListener('click', async () => {
+      const messages = document.querySelectorAll('.chat-dispute-messages');
+
       createChatWindow();
+
+      if (messages.length === 0) {
+        const chatSpinner = document.querySelector('.chat-spinner');
+        const spinnerClone = chatSpinner.cloneNode(true) as HTMLDivElement;
+        const chatMessageContainer = document.querySelector(
+          '#chat-message-container',
+        );
+        chatMessageContainer.appendChild(spinnerClone);
+        spinnerClone.classList.remove('hidden');
+        spinnerClone.style.display = 'flex';
+        spinnerClone.classList.add('chat-spinner-active');
+      }
+
       const paymentId = button.getAttribute('data-payment-id');
       const resp = await fetch('/disputes/?payment_id='.concat(paymentId));
       const roomUuid = await resp.text();
