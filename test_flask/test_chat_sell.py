c2f9fb89f0b471191a150ff5374a27af6bc26a9c
denys <denysburimov@gmai.com>
2024-02-28 18:45:07 +0200

    work: saving price

--- test_flask/test_chat_sell.py
+++ test_flask/test_chat_sell.py
@@ -2,7 +2,7 @@ import pytest
 from flask import current_app as app
 from flask.testing import FlaskClient
 from flask_login import current_user
-from app import models as m, schema as s
+from app import models as m, schema as s, db
 from test_flask.utils import login
 from app.controllers import get_event_by_name_bard
 from app.controllers.chat_sell import (
@@ -116,82 +116,24 @@ def test_get_events_from_db(client_with_data: FlaskClient):
     assert response
 
 
-# def test_chat_sell_create_event(client_with_data: FlaskClient):
-#     login(client_with_data)
-#     response = client_with_data.get("/sell/create_event")
-#     assert response.status_code == 200
-#     assert "Form submitting error" in response.data.decode()
+def test_get_ticket_price(client_with_data: FlaskClient):
+    login(client_with_data)
+    user: m.User = current_user
+    ticket: m.Ticket = db.session.scalar(m.Ticket.select())
+    ticket.event.creator_id = user.id
+    ticket.seller_id = user.id
+    ticket.price_net = None
+    ticket.price_gross = None
+    db.session.commit()
 
-#     room = m.Room(
-#         seller_id=current_user.id,
-#         buyer_id=app.config["CHAT_DEFAULT_BOT_ID"],
-#     ).save()
-#     event = db.session.scalar(m.Event.select())
-#     event_date = event.date_time.strftime(app.config["DATE_PICKER_FORMAT"])
-#     response = client_with_data.get(
-#         f"/sell/create_event?room_unique_id={room.unique_id}&event_location={event.location.name}&event_date={event_date}&event_name={event.name}&event_category={event.category.name}&event_url={event.url}"
-#     )
-#     assert response.status_code == 200
-#     assert "Please, input ticket details." in response.data.decode()
+    room = m.Room(
+        seller_id=user.id,
+        buyer_id=app.config["CHAT_DEFAULT_BOT_ID"],
+    ).save()
 
-
-# def test_chat_sell_ticket_form(client_with_data: FlaskClient):
-#     login(client_with_data)
-#     room = m.Room(
-#         seller_id=current_user.id,
-#         buyer_id=app.config["CHAT_DEFAULT_BOT_ID"],
-#     ).save()
-#     event = db.session.scalar(m.Event.select())
-#     response = client_with_data.get(f"/sell/ticket_form?room_unique_id={room.unique_id}&event_id={event.unique_id}")
-#     assert response.status_code == 200
-#     assert "Please, input ticket details." in response.data.decode()
-
-
-# def test_chat_sell_create_ticket(client_with_data: FlaskClient):
-#     login(client_with_data)
-#     room = m.Room(
-#         seller_id=current_user.id,
-#         buyer_id=app.config["CHAT_DEFAULT_BOT_ID"],
-#     ).save()
-#     event = db.session.scalar(m.Event.select())
-#     TSEC = "section"
-#     TQ = "queue"
-#     TSEAT = "seat"
-#     TQTY = 2
-#     TPRICE = 100.0
-
-#     response = client_with_data.get(
-#         f"/sell/create_ticket?room_unique_id={room.unique_id}&event_id={event.unique_id}&queue={TQ}&seat={TSEAT}&quantity={TQTY}&price={TPRICE}"
-#     )
-#     assert response.status_code == 200
-#     assert "No section provided" in response.data.decode()
-
-#     response = client_with_data.get(
-#         f"/sell/create_ticket?room_unique_id={room.unique_id}&event_id={event.unique_id}&section={TSEC}&seat={TSEAT}&quantity={TQTY}&price={TPRICE}"
-#     )
-#     assert response.status_code == 200
-#     assert "No queue provided" in response.data.decode()
-
-#     response = client_with_data.get(
-#         f"/sell/create_ticket?room_unique_id={room.unique_id}&event_id={event.unique_id}&section={TSEC}&queue={TQ}&quantity={TQTY}&price={TPRICE}"
-#     )
-#     assert response.status_code == 200
-#     assert "No seat provided" in response.data.decode()
-
-#     response = client_with_data.get(
-#         f"/sell/create_ticket?room_unique_id={room.unique_id}&event_id={event.unique_id}&section={TSEC}&queue={TQ}&seat={TSEAT}&price={TPRICE}"
-#     )
-#     assert response.status_code == 200
-#     assert "No quantity provided" in response.data.decode()
-
-#     response = client_with_data.get(
-#         f"/sell/create_ticket?room_unique_id={room.unique_id}&event_id={event.unique_id}&section={TSEC}&queue={TQ}&seat={TSEAT}&quantity={TQTY}"
-#     )
-#     assert response.status_code == 200
-#     assert "No price provided" in response.data.decode()
-
-#     response = client_with_data.get(
-#         f"/sell/create_ticket?room_unique_id={room.unique_id}&event_id={event.unique_id}&section={TSEC}&queue={TQ}&seat={TSEAT}&quantity={TQTY}&price={TPRICE}"
-#     )
-#     assert response.status_code == 200
-#     assert "You have successfully created a ticket" in response.data.decode()
+    TEST_PRICE_NET = 100
+    response = client_with_data.get(
+        f"/sell/get_ticket_price?ticket_unique_id={ticket.unique_id}&room_unique_id={room.unique_id}&user_message={TEST_PRICE_NET}"
+    )
+    assert response.status_code == 200
+    assert ticket.price_net == TEST_PRICE_NET
