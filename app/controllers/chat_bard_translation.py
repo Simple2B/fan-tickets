27e1e5ea581de314694b625a9f4247e5b51ee6f8
denys <denysburimov@gmai.com>
2024-03-27 14:49:22 +0200

    fix: translation

--- app/controllers/chat_bard_translation.py
+++ app/controllers/chat_bard_translation.py
@@ -9,7 +9,7 @@ API_KEY = os.environ.get("BARD_API_KEY")
 BARD_URL = f"https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key={API_KEY}"
 
 
-def bot_message_translation(text_en: str) -> str:
+def bot_message_translation(text_en: str) -> tuple[str, bool]:
     text_en = re.sub(r"\s+", " ", text_en)
 
     message = f"Could you please translate this text to Portuguese? {text_en}"
@@ -26,7 +26,8 @@ def bot_message_translation(text_en: str) -> str:
     try:
         bard_response = requests.post(BARD_URL, data=json.dumps(data), headers={"Content-Type": "application/json"})
         response_text = bard_response.json()["candidates"][0]["content"]["parts"][0]["text"]
+        return response_text, True
     except Exception as e:
         log(log.ERROR, "Error translating text: [%s]", e)
         response_text = text_en
-    return response_text
+        return response_text, False
